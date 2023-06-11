6f2047e308a31219068b9f270226fe35293bb789
Prashant Thakur <prashant.th02@gmail.com>
2023-06-11 14:14:17 +0530

    Added Google-Login-Feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { GoogleLogin } from "@react-oauth/google";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Box } from "@mui/system";
+import { Typography } from "@mui/material";
+
+const App = () => {
+	// Storing the jwt token in 'accessToken' state
+	const [accessToken, setAccessToken] = useState(
+		localStorage.getItem("loginData")
+			? JSON.parse(localStorage.getItem("loginData")).user.jwtToken
+			: null
+	);
+
+	// Google Login Handler
+	const handleLogin = (googleData) => {
+    
+  };
+
+	const handleError = (result) => {
+		console.log(result);
+	};
+
+	return (
+		<Box
+			sx={{
+				display: "flex",
+				justifyContent: "center",
+				minHeight: "100vh",
+				alignItems: "center",
+				backgroundColor: "#A5C9DE",
+				flexDirection: "column",
+			}}
+		>
+			<Typography variant="h2" component="h2">
+				Login With Google
+			</Typography>
+			<br />
+			<GoogleLogin onSuccess={handleLogin} onError={handleError} useOneTap />
+		</Box>
+	);
+};
 
 export default App;
